948f9f685621870dca6e4913a43266e45f012f51
YFrite <63791025+YFrite@users.noreply.github.com>
2026-02-09 09:44:07 +1000

    Update solution for cohens-kappa

--- cohens-kappa/cohens-kappa.py
+++ cohens-kappa/cohens-kappa.py
@@ -9,8 +9,12 @@ def cohens_kappa(rater1, rater2):
     p_0 = sum(rater1[k] == rater2[k] for k in range(n)) / n
     p_e = 0
 
-    for l in set(rater1):
-        p_e += (sum(rater1[k] == l for k in range(n)) / n) * (sum(rater2[k] == l for k in range(n)) / n)
+    labels = set(rater1)
+    counts1 = {l: rater1.count(l) for l in labels}
+    counts2 = {l: rater2.count(l) for l in labels}
+
+    for l in labels:
+        p_e += (counts1[l] / n) * (counts2[l] / n)
 
     if p_e == 1:
         return 1
